e444f3ac66693b687cef6d0e64f2d12bcc343f31
Rivka445 <finkelriki@gmail.com>
2026-07-20 10:10:10 +0300

    Remove dead handle_input logic from GameUIState

--- ui/state/game_ui_state.py
+++ ui/state/game_ui_state.py
@@ -9,11 +9,7 @@ class GameUIState(UIState):
     def on_enter(self) -> None: pass
     def on_exit(self) -> None: pass
 
-    def handle_input(self, event: dict) -> None:
-        if event.get("type") == "move":
-            self._bridge.send_move(event["source"], event["target"])
-        elif event.get("type") == "jump":
-            self._bridge.send_jump(event["pos"])
+    def handle_input(self, event: dict) -> None: pass
 
     def update(self, ms: int) -> None:
         self._bridge.advance_time(ms)
